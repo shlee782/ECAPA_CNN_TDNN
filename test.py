94bb3b568a571a68f3e3f14b5d927d2463b49b25
Sunghwa Lee <43434107+shlee782@users.noreply.github.com>
2023-07-07 09:32:07 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ import torch
 from models.ECAPA_CNN_TDNN import ECAPA_TDNN
 from models.ECAPA_CNN_TDNN import ECAPA_CNN_TDNN
 
-input_feats = torch.rand([5, 120, 80])
-compute_embedding = ECAPA_TDNN(80, lin_neurons=192)
-outputs = compute_embedding(input_feats)
-print(outputs.shape)
+if __name__=="__main__":
+    input_feats = torch.rand([5, 120, 80])
+    compute_embedding = ECAPA_TDNN(80, lin_neurons=192)
+    outputs = compute_embedding(input_feats)
+    print(outputs.shape)
 
-input_feats = torch.rand([5, 120, 80])
-compute_embedding = ECAPA_CNN_TDNN(int(128*80/4), lin_neurons=192) #2560=128*80/4
-outputs = compute_embedding(input_feats)
-print(outputs.shape)
+    input_feats = torch.rand([5, 120, 80])
+    compute_embedding = ECAPA_CNN_TDNN(int(128*80/4), lin_neurons=192) #2560=128*80/4
+    outputs = compute_embedding(input_feats)
+    print(outputs.shape)
